b71bd977588601784c54dd8bbfa766cd7d9aff7c
nns <niannings@163.com>
2018-06-26 16:47:06 +0800

    tywl-vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  base: '/tywl/',
+  title: '天羽蔚灵',
+  description: '天羽蔚灵@一不小心'
+
+}
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="app">
+    HELLO
+  </div>
+</template>
+
+<style></style>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
